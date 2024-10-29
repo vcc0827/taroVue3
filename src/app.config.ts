36fb6fccb55e10b57790efdb351b1de1af35154b
vcc0827 <vcc0827@github.com>
2024-10-29 18:57:54 +0800

    feat: custom-tab-bar

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,39 +1,47 @@
 export default {
   pages: [
-    "pages/index/index",
     "pages/recommend/index",
+    "pages/index/index",
     "pages/home/index",
     "pages/message/index",
     "pages/mine/index",
   ],
-  tabBar: {
-    custom: true,
-    color: "#f0f0f0",
-    selectedColor: "#000",
-    backgroundColor: "#000",
-    list: [
-      {
-        pagePath: "pages/recommend/index",
-        text: "推荐",
-      },
-      {
-        pagePath: "pages/home/index",
-        text: "单身库",
-      },
-      {
-        pagePath: "pages/message/index",
-        text: "信息",
-      },
-      {
-        pagePath: "pages/mine/index",
-        text: "我的",
-      },
-    ],
-  },
   window: {
     backgroundTextStyle: "light",
     navigationBarBackgroundColor: "#fff",
     navigationBarTitleText: "WeChat",
     navigationBarTextStyle: "black",
   },
+  tabBar: {
+    custom: false,
+    color: '#000000',
+    selectedColor: '#DC143C',
+    backgroundColor: '#ffffff',
+    list: [
+      {
+        pagePath: 'pages/recommend/index',
+        selectedIconPath: 'images/tabbar_home_on.png',
+        iconPath: 'images/tabbar_home.png',
+        text: '推荐'
+      },
+      {
+        pagePath: 'pages/home/index',
+        selectedIconPath: 'images/tabbar_cate_on.png',
+        iconPath: 'images/tabbar_cate.png',
+        text: '单身库'
+      },
+      {
+        pagePath: 'pages/message/index',
+        selectedIconPath: 'images/tabbar_cart_on.png',
+        iconPath: 'images/tabbar_cart.png',
+        text: '消息'
+      },
+      {
+        pagePath: 'pages/mine/index',
+        selectedIconPath: 'images/tabbar_my_on.png',
+        iconPath: 'images/tabbar_my.png',
+        text: '我的'
+      }
+    ]
+  }
 };
--- src/pages/custom-tab-bar/index.vue
+++ src/pages/custom-tab-bar/index.vue
@@ -20,11 +20,11 @@
 
 <script setup>
 import Taro from "@tarojs/taro";
-import { computed } from "vue";
-import { useStore } from "vuex";// 改成pinia
+import { useTabBarStore } from "../stores/tabBar.ts";
 
-const store = useStore();
-const selected = computed(() => store.getters.getSelected);
+const store = useTabBarStore();
+const selected = store.selected;
+console.log("selected:", selected);
 
 const color = "#000000";
 const selectedColor = "#DC143C";
@@ -33,36 +33,37 @@ const list = [
     pagePath: "/pages/recommend/index",
     selectedIconPath: "../images/tabbar_home_on.png",
     iconPath: "../images/tabbar_home.png",
-    text: "首页",
+    text: "推荐",
   },
   {
-    pagePath: "/pages/cate/index",
+    pagePath: "/pages/home/index",
     selectedIconPath: "../images/tabbar_cate_on.png",
     iconPath: "../images/tabbar_cate.png",
-    text: "分类",
+    text: "单身库",
   },
   {
-    pagePath: "/pages/cart/index",
+    pagePath: "/pages/message/index",
     selectedIconPath: "../images/tabbar_cart_on.png",
     iconPath: "../images/tabbar_cart.png",
-    text: "购物车",
+    text: "消息",
   },
   {
-    pagePath: "/pages/my/index",
+    pagePath: "/pages/mine/index",
     selectedIconPath: "../images/tabbar_my_on.png",
     iconPath: "../images/tabbar_my.png",
-    text: "个人中心",
+    text: "我的",
   },
 ];
 
-function switchTab(index, url) {
+const switchTab = (index, url) => {
+  console.log('index:', index,url);
   setSelected(index);
   Taro.switchTab({ url });
-}
+};
 
-function setSelected(index) {
-  store.dispatch("setSelected", index);
-}
+const setSelected = (index) => {
+  store.setSelected(index);
+};
 </script>
 
 <style lang="scss">
